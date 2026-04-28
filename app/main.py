c8211f980dd7e1b09482e0f4da998f9d8aa8608b
siinzn <msinannoufal@gmail.com>
2026-04-28 18:45:10 +0400

    Removed ratelimiting

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ from app.modules.online_deals.router import router as online_deals_router
 from app.modules.app_status.router import router as app_status_router
 from app.core.config import Settings
 from app.core.middleware import SecurityHeadersMiddleware, RequestSizeLimitMiddleware, LoggingMiddleware
-from app.core.ratelimit import RateLimitMiddleware
+
 
 def create_app() -> FastAPI:
     """
@@ -87,7 +87,7 @@ def create_app() -> FastAPI:
     # Security Middlewares
     # ================================
     app.add_middleware(SecurityHeadersMiddleware)
-    app.add_middleware(RateLimitMiddleware)
+
     app.add_middleware(RequestSizeLimitMiddleware, max_upload_size=settings_obj.MAX_UPLOAD_SIZE_BYTES)
     app.add_middleware(LoggingMiddleware)
 
